588a7a263127ce47eec2762fcb4ab64b6fa3b76d
Kevin Ghadyani <Saturn2888@gmail.com>
2018-11-21 06:56:05 -0600

    Stubbed out the CRUD actions

--- redux/httpServers/utils/getHttpServerConfig.js
+++ redux/httpServers/utils/getHttpServerConfig.js
@@ -20,10 +20,44 @@ const getHttpServerConfig = (
 		)
 	)
 	.get(
-		'/test',
+		'/subscriptions',
 		dispatchableRequest(
 			getTest
-		)
+		),
+	)
+	.get(
+		'/subscriptions/:subscriptionId',
+		dispatchableRequest(
+			getTest
+		),
+	)
+	.post(
+		'/subscriptions',
+		dispatchableRequest(
+			getTest
+		),
+	)
+
+	// Subscriptions may require updates such as changing how often to be notified.
+	// In this PoC, I've opted to exclude this functionality as it adds a significant amount of scope-creep.
+	// .put(
+	// 	'/subscriptions/:subscriptionId',
+	// 	dispatchableRequest(
+	// 		updateSubscription
+	// 	),
+	// )
+
+	.delete(
+		'/subscriptions',
+		dispatchableRequest(
+			getTest
+		),
+	)
+	.delete(
+		'/subscriptions/:subscriptionId',
+		dispatchableRequest(
+			getTest
+		),
 	)
 )
 
