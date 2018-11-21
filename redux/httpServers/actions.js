8f116439df563a41350f8722259a49996c463706
Kevin Ghadyani <Saturn2888@gmail.com>
2018-11-21 07:29:22 -0600

    Removed GET_TEST action from httpServers

--- redux/httpServers/actions.js
+++ redux/httpServers/actions.js
@@ -1,5 +1,4 @@
 const ADD_HTTP_SERVER = 'HTTP_SERVERS::ADD_HTTP_SERVER'
-const GET_TEST = 'HTTP_SERVERS::GET_TEST'
 const SEND_RESPONSE = 'HTTP_SERVERS::SEND_RESPONSE'
 
 const addHttpServer = (
@@ -7,15 +6,6 @@ const addHttpServer = (
 ) => ({
 	server,
 	type: ADD_HTTP_SERVER,
-})
-
-const getTest = ({
-	request,
-	response,
-}) => ({
-	request,
-	response,
-	type: GET_TEST,
 })
 
 const sendResponse = ({
@@ -30,8 +20,6 @@ const sendResponse = ({
 module.exports = {
 	ADD_HTTP_SERVER,
 	addHttpServer,
-	GET_TEST,
-	getTest,
 	SEND_RESPONSE,
 	sendResponse,
 }
