ab3697bc763c1239ed20b3f88226b9f68dde5e2e
Kevin Ghadyani <Saturn2888@gmail.com>
2018-11-21 09:13:11 -0600

    Added ability to add subscriptions to the database

--- redux/subscriptions/addSubscriptionEpic.js
+++ redux/subscriptions/addSubscriptionEpic.js
@@ -1,8 +1,10 @@
-const { map } = require('rxjs/operators')
+const { map, mergeMap, take } = require('rxjs/operators')
+const { merge, of } = require('rxjs')
 const { ofType } = require('redux-observable')
 
-const { sendResponse } = require('$redux/httpServers/actions')
+const sendDatabaseResponse = require('$redux/utils/sendDatabaseResponse')
 const { ADD_SUBSCRIPTION } = require('./actions')
+const { addDatabaseEntries } = require('$redux/dataStorage/actions')
 
 const addSubscriptionEpic = (
 	action$,
@@ -10,7 +12,52 @@ const addSubscriptionEpic = (
 	action$
 	.pipe(
 		ofType(ADD_SUBSCRIPTION),
-		map(sendResponse),
+		map(({
+			request,
+			response,
+		}) => ({
+			response,
+			storageActionId: Symbol(),
+			subscriptionId: (
+				request
+				.body
+				.subscriptionId
+			),
+			userId: (
+				request
+				.headers['user-id']
+			),
+		})),
+		mergeMap(({
+			response,
+			storageActionId,
+			subscriptionId,
+			userId,
+		}) => (
+			merge(
+				(
+					sendDatabaseResponse({
+						action$,
+						storageActionId,
+						response,
+					})
+				),
+				(
+					of(
+						addDatabaseEntries({
+							entries: [{
+								subscriptionId,
+								userId,
+							}],
+							storageActionId,
+						})
+					)
+				),
+			)
+			.pipe(
+				take(2),
+			)
+		)),
 	)
 )
 
