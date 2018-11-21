7fee407b8baa8dfe325d4b1b54e41892779b7e4a
Kevin Ghadyani <Saturn2888@gmail.com>
2018-11-21 09:17:12 -0600

    Stubbed out rest of API actions

--- redux/subscriptions/actions.js
+++ redux/subscriptions/actions.js
@@ -1,4 +1,8 @@
 const ADD_SUBSCRIPTION = 'SUBSCRIPTIONS::ADD_SUBSCRIPTION'
+const GET_SUBSCRIPTION = 'SUBSCRIPTIONS::GET_SUBSCRIPTION'
+const GET_SUBSCRIPTIONS = 'SUBSCRIPTIONS::GET_SUBSCRIPTIONS'
+const REMOVE_SUBSCRIPTION = 'SUBSCRIPTIONS::REMOVE_SUBSCRIPTION'
+const REMOVE_SUBSCRIPTIONS = 'SUBSCRIPTIONS::REMOVE_SUBSCRIPTIONS'
 
 const addSubscription = ({
 	request,
@@ -9,12 +13,51 @@ const addSubscription = ({
 	type: ADD_SUBSCRIPTION,
 })
 
+const getSubscription = ({
+	request,
+	response,
+}) => ({
+	request,
+	response,
+	type: GET_SUBSCRIPTION,
+})
+
+const getSubscriptions = ({
+	request,
+	response,
+}) => ({
+	request,
+	response,
+	type: GET_SUBSCRIPTIONS,
+})
+
+const removeSubscription = ({
+	request,
+	response,
+}) => ({
+	request,
+	response,
+	type: REMOVE_SUBSCRIPTION,
+})
+
+const removeSubscriptions = ({
+	request,
+	response,
+}) => ({
+	request,
+	response,
+	type: REMOVE_SUBSCRIPTIONS,
+})
+
 module.exports = {
 	ADD_SUBSCRIPTION,
 	addSubscription,
-
-	// getSubscription,
-	// getSubscriptions,
-	// removeSubscription,
-	// removeSubscriptions,
+	GET_SUBSCRIPTION,
+	GET_SUBSCRIPTIONS,
+	getSubscription,
+	getSubscriptions,
+	REMOVE_SUBSCRIPTION,
+	REMOVE_SUBSCRIPTIONS,
+	removeSubscription,
+	removeSubscriptions,
 }
